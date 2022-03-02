80118c7cd443100c471d76380501e125f7d41dfa
Jonatanortiz2 <59237369+Jonatanortiz2@users.noreply.github.com>
2022-03-02 03:40:51 -0500

    Create Laser x.lua

---
+++ Laser x.lua
@@ -0,0 +1,8 @@
+--[[
+    Don't Touch Anything Of This, Including The 5.1!!!!!!!!!, IF YOU WANT YOUR SRIPT TO BE BROKEN THAN CHANGE IT
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jonatanortiz2/Johnny-Test/master/freelaserxpart1.lua"))();
+wait(5.1)
+game:GetService("RunService").Heartbeat:Connect(function()
+	_G.UpdatePosition()
+end)
