221d9f8415875a9bbc30c154b21f152012391a66
Jonatanortiz2 <59237369+Jonatanortiz2@users.noreply.github.com>
2022-12-24 17:58:10 -0500

    Update Ui-Lib.lua

--- Ui-Lib.lua
+++ Ui-Lib.lua
@@ -2120,7 +2120,7 @@ function Library:CreateWindow(HubName, GameName, IntroText, IntroIcon, ImprovePe
                             BackgroundTransparency = 1,
                             Position = UDim2.new(0, 374, 0, 3),
                             Size = UDim2.new(0, 25, 0, 25),
-                            Image = 'rbxassetid://3926307971',
+                            Image = 'rbxassetid://4921682252',
                             ImageColor3 = Theme.SecondaryTextColor,
                             ImageRectOffset = Vector2.new(564, 364),
                             ImageRectSize = Vector2.new(36, 36)
